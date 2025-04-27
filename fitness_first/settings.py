b057369ee4471a6ae55656020b46801c7efdea8a
godriceeichie <emmaeichie@gmail.com>
2025-04-27 20:17:00 +0100

    fixed the static root error

--- fitness_first/settings.py
+++ fitness_first/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-as2^6^$-#g=k&u*chl#22c-ba!_fca7@!eb)67v-g9gr3_txl!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['fitness-first-ybbz.onrender.com']
+ALLOWED_HOSTS = ['fitness-first-ybbz.onrender.com', 'localhost']
 
 
 # Application definition
@@ -111,7 +111,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
